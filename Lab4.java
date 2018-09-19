cd64735f1b746f91d4b74f40475eb2d6ed74b583
Taryn <hainskarmilta@vcu.edu>
2018-09-19 12:41:56 -0400

    changes made

---
+++ Lab4.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+
+public class Lab4 {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	/* Method to receive and sort an array of numbers
+	 * 
+	 */
+	public int sort(int input ) {
+		try {
+	    int[] array1 = new int[]{3, 4, 5, 65, 98,45};
+	    int[] array2 = new int[]{3, 43, 5, 6, 98,45};
+	    int[] array3 = new int[]{35, 4, 5, 6, 98,45};
+	    int[] array4 = new int[]{3, 4, 54, 6, 98,45};
+	    int[] array5 = new int[]{3, 4, 5, 6, 98,450};
+	    ArrayList<Integer> list = new ArrayList<Integer>();
+	    list.add(array1);
+
+	   
+	   
+	    
+	   
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Invalid data, please try again");
+			
+		}
+		return input;
+		 
+	}
+}
